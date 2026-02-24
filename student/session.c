afe88a17da59c8d779e1518777e2aa607d4988b5
joelmena1423 <joel.mena01@estudiant.upf.edu>
2026-02-24 11:00:01 +0100

    CODI session.c COPIAT

--- student/session.c
+++ student/session.c
@@ -2,22 +2,48 @@
 
 // LAB 1 - functions 
 void restart_session_game(Session *session){
-    // ToDo - Lab 1 & refactor in Lab 2
+    //OBJECTIU DE LA FUNCIÓ --> Finalitzar la partida i reiniciar-la de nou
+    if(session != NULL){        //OBSERVEM SI LA SESSIÓ ACTUAL JA ESTÀ EN PARTIDA O NO (SI EN CARA NO HA COMENÇAT NO CAL FINALITZAR-LA, SINÓ QUE INICIAR-LA)
+        free_session(session);  //FINALITZEM LA PARTIDA ACTUAL
+    }
+    init_game(&session ->current_game); //INICIEM LA PARTIDA DE NOU
 }
 
+
 void init_session(Session *session){
-    // ToDo - Lab 1 & refactor in Lab 2
+    for(int level = 0; level < MAX_LEVELS; level++){ //RECORREM TOTS ELS NIVELES I INICIALITZEM LA MILLOR PUNTUACIÓ A 0
+        session ->best_score[level] = 0;//INICIALITZEM LA MILLOR PUNTUACIÓ A 0
+    }
+    init_game(&session ->current_game);//INICIALITZEM LA PARTIDA ACTUAL
 }
 
 void print_session(Session *session){
-    // ToDo - Lab 1
+    unsigned level = session -> current_game.level;
+    printf("Millor puntuació: %d\n", session-> best_score[level]);
+    print_game(session -> current_game);
 }
 
+
 void new_game_score(Session *session){
-    // ToDo - Lab 1
+     unsigned level = session -> current_game.level;
+    int score = session -> current_game.score;
+    if (session -> best_score[level] == 0){
+        session -> best_score[level] = score;
+    }
+
+    else if(score < session -> best_score[level]){ 
+        session -> best_score[level] = score;
+    }
 }
 
 // LAB 2 - functions
 void free_session(Session *s){
-	// ToDo - Lab 2
+	
+    //OBJECTIU HA DE SER ALLIBERAR LA MEMÒRIA OCUPADA PER AQUESTA PARTIDA
+    //s->current_game.state.grid;
+
+    if(s != NULL){
+        free_game(&(s->current_game));
+    }
+
 }
